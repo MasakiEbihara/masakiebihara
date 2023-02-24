78ffea2b9c9378d4ea2e22421f3d6c4fb2b98584
MasakiEbihara <masaki.ebi0928@gmail.com>
2023-02-24 17:00:54 +0900

    マインスイーパー

---
+++ Assets/Cell.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CellState
+{
+    None = 0,
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+
+    Mine = -1,
+}
+public class Cell : MonoBehaviour
+{
+    [SerializeField]
+    private Text _view = null;
+
+    [SerializeField]
+    private CellState _cellState = CellState.None;
+    public CellState CellState //セルの状態を別のスクリプトからも更新できるように
+    {
+        get => _cellState;
+        set
+        {
+            _cellState = value;
+            OnCellStateChanged();
+        }
+    }
+
+
+
+    private void OnValidate()
+    {
+        OnCellStateChanged();
+    }
+
+    private void OnCellStateChanged()
+    {
+        if (_view == null) { return; }
+
+        if(_cellState == CellState.None)
+        {
+            _view.text = "";
+        }
+        else if(_cellState == CellState.Mine)
+        {
+            _view.text = "×";
+            _view.color = Color.red;
+        }
+        else
+        {
+            _view.text = ((int)_cellState).ToString();
+            _view.color = Color.blue;
+        }
+
+
+
+    }
+}
